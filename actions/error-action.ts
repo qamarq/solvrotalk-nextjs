e5ea5b2bd97d16e2680a54b889b14b6f646cab91
QamarQ <kamilm@you2.pl>
2025-03-09 14:37:43 +0100

    fix: ?

--- actions/error-action.ts
+++ actions/error-action.ts
@@ -1,7 +1,7 @@
 'use server'
 
 export const exampleErrorFunction = async () => {
-  const triggerError = Math.random() > 0.5
+  const triggerError = Math.random() > 1
   if (triggerError) {
     throw new Error('Error triggered')
   }
